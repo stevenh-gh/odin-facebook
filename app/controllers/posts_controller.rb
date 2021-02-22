d62f52947939d105ca1db4aa964d93fff31218fe
steven <steven.h@live.com>
2021-02-21 18:19:25 -0800

    add new and create actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,23 @@ class PostsController < ApplicationController
   def index
     @posts = User.where(id: params[:user_id]).last.posts
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to(user_posts_path)
+    else
+      render(:new)
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:text, :user_id)
+  end
 end
