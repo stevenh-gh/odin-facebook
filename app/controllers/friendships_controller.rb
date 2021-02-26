0a010763e7b71a35da5cb739072c2f94edda4949
steven <steven.h@live.com>
2021-02-25 22:34:44 -0800

    add ability to make mutual friends

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,9 +1,22 @@
 class FriendshipsController < ApplicationController
   def create
-
+    # @friendship = Friendship.new(user_id: current_user.id, friend_id: params[:friend_id])
+    # if @friendship.save
+    #   flash[:success] = "#{User.find(params[:friend_id]).name} has been added as friend."
+    #   redirect_to(root_path)
+    # else
+    #   flash[:error] = @friendship.errors.full_path
+    #   redirect_to(root)
+    # end
+    friendships = Friendship.make_friendship(current_user.id, params[:friend_id])
+    if friendships[0].save && friendships[1].save
+      flash[:success] = "#{User.find(params[:friend_id]).name} has been added as friend."
+      redirect_to(root_path)
+    else
+      flash[:error] = friendships[0].errors.full_path
+      redirect_to(root_path)
+    end
   end
 
-  def destroy
-
-  end
+  def destroy; end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,10 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def self.make_friendship(user_id, friend_id)
+    user_friendship = Friendship.new(user_id: user_id, friend_id: friend_id)
+    reverse = Friendship.new(user_id: friend_id, friend_id: user_id)
+    [user_friendship, reverse]
+  end
 end
