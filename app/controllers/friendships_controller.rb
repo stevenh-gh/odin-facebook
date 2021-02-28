182f51756797516c6e2dcef0486e42defbe822cb
steven <steven.h@live.com>
2021-02-27 17:08:55 -0800

    add functionality to delete friend request from db after acceptance

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -11,6 +11,7 @@ class FriendshipsController < ApplicationController
     friendships = Friendship.make_friendship(current_user.id, params[:friend_id])
     if friendships[0].save && friendships[1].save
       flash[:success] = "#{User.find(params[:friend_id]).name} has been added as friend."
+      FriendRequest.destroy(FriendRequest.where(sender_id: params[:friend_id], receiver_id: current_user.id).first.id)
       redirect_to(root_path)
     else
       flash[:error] = friendships[0].errors.full_path
