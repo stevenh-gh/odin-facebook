45cc394cbc11368f4b3b1561c3365fd769eb2e59
steven <steven.h@live.com>
2021-03-01 17:22:35 -0800

    add likes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for(:users, controllers: { registrations: 'registrations' })
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources(:users) do
-    resources(:posts)
+    resources(:posts) do
+      resources(:likes)
+    end
     resources(:comments)
   end
   resources(:friendships, only: %i[index create destroy])
