ec93defe2c5a884b1b8bf4f4d669210b97f29317
steven <steven.h@live.com>
2021-03-02 16:30:47 -0800

    checks if user has already liked a post before user can submit a like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,21 @@
 class LikesController < ApplicationController
+  before_action(:check_liked, only: :create)
+
   def create
-    @like=current_user.likes.build(post_id: params[:post_id])
+    @like = current_user.likes.build(post_id: params[:post_id])
     if @like.save
       redirect_back(fallback_location: root_path)
     else
       redirect_to(root_path)
+    end
+  end
+
+  private
+
+  def check_liked
+    if Like.exists?(user_id: current_user.id)
+      flash[:error] = 'You already liked this post!'
+      redirect_back(fallback_location: root_path)
     end
   end
 end
