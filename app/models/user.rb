de25a8b81ccaece33b03af224d846cafb0590a6f
steven <steven.h@live.com>
2021-03-06 19:42:09 -0800

    welcome email sends to user after registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,10 +18,15 @@ class User < ApplicationRecord
   has_many(:friend_requests, foreign_key: 'receiver_id')
 
   before_create(:build_profile)
+  after_create(:send_welcome_email)
 
   private
 
   def build_profile_before_create
     build_profile
+  end
+
+  def send_welcome_email
+    UserMailer.with(user: self).welcome_email.deliver_later
   end
 end
